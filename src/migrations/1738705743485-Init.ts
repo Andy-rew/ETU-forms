c49e11c4cf15286b1bacb863c353d942a5ad9427
andy <chernyshev@spiks.ru>
2025-03-05 14:31:35 +0300

    fix: lint

--- src/migrations/1738705743485-Init.ts
+++ src/migrations/1738705743485-Init.ts
@@ -1,16 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1738705743485 implements MigrationInterface {
-    name = 'Init1738705743485'
+  name = 'Init1738705743485';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "form_schema_user_templates" DROP COLUMN "title"`);
-        await queryRunner.query(`ALTER TABLE "form_schemas" ADD "title" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "form_schema_user_templates" DROP COLUMN "title"`);
+    await queryRunner.query(`ALTER TABLE "form_schemas" ADD "title" character varying NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "form_schemas" DROP COLUMN "title"`);
-        await queryRunner.query(`ALTER TABLE "form_schema_user_templates" ADD "title" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "form_schemas" DROP COLUMN "title"`);
+    await queryRunner.query(`ALTER TABLE "form_schema_user_templates" ADD "title" character varying NOT NULL`);
+  }
 }
