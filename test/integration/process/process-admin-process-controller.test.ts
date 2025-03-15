365515ddc8514e6f7973afd1b80c1fe5ff59825a
andy <chernyshev@spiks.ru>
2025-03-15 15:31:02 +0300

    feat: test add userss

--- test/integration/process/process-admin-process-controller.test.ts
+++ test/integration/process/process-admin-process-controller.test.ts
@@ -7,6 +7,9 @@ import { ProcessAdminProcessCreateDto } from '@applications/http/process-admin/p
 import * as dayjs from 'dayjs';
 import { CommonProcessService } from '@domain/process/services/common-process.service';
 import { ProcessAdminProcessEditDto } from '@applications/http/process-admin/process/request/process-admin-process-edit.dto';
+import { ProcessAdminProcessUsersAddDto } from '@applications/http/process-admin/process/request/process-admin-process-users-add.dto';
+import { ProcessUserRoleEnum } from '@domain/process/enums/process-user-role.enum';
+import { CommonStepService } from '@domain/step/services/common-step.service';
 
 @suite()
 export class ProcessAdminProcessControllerTest extends BaseTestClass {
@@ -172,6 +175,79 @@ export class ProcessAdminProcessControllerTest extends BaseTestClass {
     const res = await this.httpRequest().withAuth(user).post('/process-admin/process/edit').body(body).execute();
 
     expect(res.status).toBe(403);
+  }
+
+  @test()
+  async addUsersToProcessSuccess() {
+    const processAdmin = await this.getBuilder(UserBuilder)
+      .withRoles([UserRoleEnum.processAdmin])
+      .withStatus(UserStatusEnum.activated)
+      .build();
+
+    const dataForCreation = {
+      title: 'Тестовый процесс',
+      startDate: new Date(),
+      endDate: new Date(),
+    };
+
+    const process = await this.getService(CommonProcessService).create({
+      title: dataForCreation.title,
+      startDate: dataForCreation.startDate,
+      endDate: dataForCreation.endDate,
+      processAdmin: processAdmin,
+    });
+
+    const defaultStepData = {
+      processId: process.id,
+      title: 'Тестовый этап',
+      startTime: dayjs().add(1, 'day').toDate(),
+      endTime: dayjs().add(2, 'day').toDate(),
+      participantsCount: null,
+      parentId: null,
+    };
+
+    const step = await this.getService(CommonStepService).create({
+      ...defaultStepData,
+      title: 'Тестовый этап 1',
+    });
+
+    const usersToAdd = await this.getBuilder(UserBuilder).buildMany(5);
+
+    const body: ProcessAdminProcessUsersAddDto = {
+      userType: ProcessUserRoleEnum.participant,
+      processId: process.id,
+      emails: usersToAdd.map((user) => user.email),
+      stepId: null,
+    };
+
+    const resCheckParticipantsAdd = await this.httpRequest()
+      .withAuth(processAdmin)
+      .post('/process-admin/process/users/add')
+      .body(body)
+      .execute();
+
+    expect(resCheckParticipantsAdd.status).toBe(201);
+
+    body.userType = ProcessUserRoleEnum.manager;
+
+    const resCheckManagersAdd = await this.httpRequest()
+      .withAuth(processAdmin)
+      .post('/process-admin/process/users/add')
+      .body(body)
+      .execute();
+
+    expect(resCheckManagersAdd.status).toBe(201);
+
+    body.userType = ProcessUserRoleEnum.expert;
+    body.stepId = step.id;
+
+    const resCheckExpertsAdd = await this.httpRequest()
+      .withAuth(processAdmin)
+      .post('/process-admin/process/users/add')
+      .body(body)
+      .execute();
+
+    expect(resCheckExpertsAdd.status).toBe(201);
   }
 }
 describe('', () => {});
